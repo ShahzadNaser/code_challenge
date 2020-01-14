bc6d16e44747b1082b977e1627adc26fb38ce619
Shahzad Naser <shahzadnasir1122@gmail.com>
2020-01-15 00:18:50 +0500

    complete import json and upadate in case of dublicate entry

--- application/models/Sales_model.php
+++ application/models/Sales_model.php
@@ -32,11 +32,58 @@ class Sales_model extends CI_Model {
 
         return $this->db->get()->result_array();
     }
+    /**
+     * This function insert new product and update if already exist.
+    */
+    public function save($data){
+        $sale = [];
+        if(array_key_exists('id', $data)){
+            $sale = $this->_get(array("id"=>$data['id']));
+        }
+        if($sale){
+            $this->update($data['id'],$data);
+        }else{
+            $this->db->insert($this->tblsales,$data);
+        }
+    }
 
+    /**
+     * This function update sales details.
+    */
+    private function update($id,$data){
+        $data['updated_at'] = date('Y-m-d h:i:s');
+        $this->db->where('id',$id);
+        $this->db->update($this->tblsales,$data);
+    }
+    /**
+     * This function return sale data if exist.
+    */
+    private function _get($where){
+        return $this->db->where($where)
+            ->get($this->tblsales)
+            ->row_array();
+    }
     /**
      * This function add/save sales data and  and update or add new customer/products if needed.
     */
     public function import_sales($sales){
+        $CI =& get_instance();
+        $CI->load->model('Customer_model','cm');
+        $CI->load->model('Product_model','pm');
+        try{
+            foreach($sales as $sale){
+                $data = array(
+                    'id'            => $sale['sale_id'],
+                    'customer_id'   =>  $CI->cm->save(array("name"=>$sale["customer_name"],"email"=>$sale["customer_mail"])),
+                    'product_id'    =>  $CI->pm->save(array("id"=>$sale["product_id"],"name"=>$sale[(isset($sale['product_name'])?'product_name':'name')],"price"=>$sale["product_price"])),
+                    'created_at'    => $sale['sale_date']
+                );
+                $this->save($data);
+            }
+            return true;
+        }catch (Exception $e){
+            return $e->getMessage();
+        }
 
     }
 }
