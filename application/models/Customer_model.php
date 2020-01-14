8cf5a375335bd04bb5e47ae79ef12fb1552b01e9
Shahzad Naser <shahzadnasir1122@gmail.com>
2020-01-14 23:14:28 +0500

    add basic save/update functions for customer and product

--- application/models/Customer_model.php
+++ application/models/Customer_model.php
@@ -3,5 +3,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Customer_model extends CI_Model {
     private $tblcustomers = CUSTOMERS_TABLE;
-	
+	/**
+     * This function insert new customer and update if already exist.
+    */
+    public function save($data){
+        if(array_key_exists('email', $data)){
+            $customer = $this->_get(array("email"=>$data['email']));
+        }
+        if(!$customer){
+            $this->db->insert($this->tblcustomers,$data);
+            return $this->db->insert_id();
+        }else{
+            $this->update($customer['id'],$data);
+        }
+        return $customer['id'];
+    }
+	/**
+     * This function update customer details.
+    */
+    private function update($id,$data){
+        $data['updated_at'] = date('Y-m-d h:i:s');
+        $this->db->where('id',$id);
+        $this->db->update($this->tblcustomers,$data);
+    }
+    /**
+     * This function return customer record according to params
+    */
+    private function _get($where){
+        return $this->db->where($where)
+            ->get($this->tblcustomers)
+            ->row_array();
+    }
 }
--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -4,5 +4,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Product_model extends CI_Model {
     private $tblproducts = PRODUCTS_TABLE;
     
-	
+	/**
+     * This function insert new product and update if already exist.
+    */
+    public function save($data){
+        if(array_key_exists('id', $data)){
+            $product = $this->_get(array("id"=>$data['id']));
+        }
+        if(!$product){
+            $this->db->insert($this->tblproducts,$data);
+            return $this->db->insert_id();
+        }else{
+            unset($data['id']);
+            $this->update($product['id'],$data);
+        }
+        return $product['id'];
+    }
+	/**
+     * This function update product details.
+    */
+    private function update($id,$data){
+        $data['updated_at'] = date('Y-m-d h:i:s');
+        $this->db->where('id',$id);
+        $this->db->update($this->tblproducts,$data);
+    }
+    /**
+     * This function return product record according to params
+    */
+    private function _get($where){
+        return $this->db->where($where)
+            ->get($this->tblproducts)
+            ->row_array();
+    }
 }
