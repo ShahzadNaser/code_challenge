77b5eee92c641bf83ec33d9ff28da2ed38f4025f
Shahzad Naser <shahzadnasir1122@gmail.com>
2020-01-14 22:01:40 +0500

    add title dynamically and add meta information

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -1,2 +1,11 @@
 <?php
 
+if ( !function_exists('get_meta')) {
+    function get_meta(){
+        return array(
+			array('name' => 'description','content' => 'Rexx Systems Coding Challenge'),
+			array('name' => 'keywords','content' => 'rexx, coding, challege','filter','search'),
+			array('name' => 'Content-type','content' => 'text/html; charset=utf-8','type' => 'equiv')
+		);
+    }
+}
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -2,6 +2,8 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <?= meta(get_meta()); ?>
+        <title><?= $title; ?></title>
         <?= link_tag(array('href'=> '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', 'rel'=> 'stylesheet')); ?>
         <?= link_tag(array('href'=> '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', 'rel'=> 'stylesheet')); ?>
         <?= link_tag(array('href'=> base_url('static/css/styles.css'), 'rel'=> 'stylesheet', 'type'=> 'text/css')); ?>
