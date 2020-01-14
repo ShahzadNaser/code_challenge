19fcfe6a3297551758c07c8a5c9df1b0ee86d5fb
Shahzad Naser <shahzadnasir1122@gmail.com>
2020-01-14 22:01:55 +0500

    update controller

--- application/controllers/Challenge.php
+++ application/controllers/Challenge.php
@@ -2,24 +2,26 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Challenge extends CI_Controller {
-
+	public function __construct()
+    {
+        parent::__construct();
+	}
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+     * This function to load default pages for the code challenge with sales table with filters/
+    */
 	public function index()
 	{
-		$this->load->view('index');
+		$data['title'] = 'Sales rexx Systems Coding Challege';
+		$data['content']  = $this->load->view('pages/sales',$data,true);
+		$this->load->view('index',$data);
 	}
+	/**
+     * This function loads import page.
+    */
+	public function import(){
+		$data['title'] = 'import-Sales rexx  Systems Coding Challege';
+		$data['content']  = $this->load->view('pages/import',$data,true);
+		$this->load->view('index',$data);
+	}
+
 }
