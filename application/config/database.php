d56a1d28c9df167938c17a36ca9effb7d39bdde4
Shahzad Naser <shahzadnasir1122@gmail.com>
2020-01-14 21:23:33 +0500

    add basic configurations

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'mysql',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'code_challenge_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
